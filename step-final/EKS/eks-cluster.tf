a43259b57b43c401220f444a4ab1cd00e67eebf2
Valeriy Rusnak <xacan11@gmail.com>
2024-11-25 19:50:36 +0200

    changed CoreDNS version in eks-cluster.tf

--- step-final/EKS/eks-cluster.tf
+++ step-final/EKS/eks-cluster.tf
@@ -24,7 +24,8 @@ data "aws_eks_cluster_auth" "danit" {
 resource "aws_eks_addon" "coredns" {
   cluster_name                = var.name
   addon_name                  = "coredns"
-  addon_version               = "v1.11.1-eksbuild.4"
+  #addon_version               = "v1.11.1-eksbuild.4"
+  addon_version		      = "v1.11.3-eksbuild.1"
   resolve_conflicts_on_create = "OVERWRITE"
 
   depends_on = [aws_eks_node_group.danit-amd]
